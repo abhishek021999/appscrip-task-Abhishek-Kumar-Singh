066e98d2ef4d6b26330c78f78805ade32444b425
Abhishek Kumar Singh <107998125+abhishek021999@users.noreply.github.com>
2024-08-11 21:46:09 +0530

    added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from '../components/navbar'
-import Footer_F from "../components/Footer_F"
+import Navbar from '@/components/navbar'
+import Footer_F from "@/components/Footer_F"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
